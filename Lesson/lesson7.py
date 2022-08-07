ff6943efc5d30446dbea9d2d36f89cbfe82abaaa
hitohito1023 <ryotakinase1023@gmail.com>
2022-08-07 10:41:16 +0900

    [save]途中保存

---
+++ Lesson/lesson7.py
@@ -0,0 +1,36 @@
+# name = "Ted"
+# for character in name:
+#   print(character)
+
+# shows = ["GOT", "Narcos", "Vice"]
+# for show in shows:
+#   print(show)
+
+# coms = ("A. Development", "Friends", "Always Sunny")
+# for show in coms:
+#   print(show)
+
+# people = {"G. Bluth II": "A. Development",
+#           "Barney": "HIMYM",
+#           "Dennis": "Always Sunny"
+#         }
+# for character in people:
+#   print(people[character])
+
+# tv = ["GOT", "Narcos", "Vice"]
+# i = 0
+# for show in tv:
+#   new = tv[i]
+#   new = new.upper()
+#   tv[i] = new
+#   i += 1
+# print(tv)
+
+for i in range(1, 11):
+  print(i)
+
+x = 10
+while x > 0:
+  print('{}'.format(x))
+  x -= 1
+print("Happy New Year!")
