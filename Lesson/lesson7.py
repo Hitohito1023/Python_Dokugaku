b7e7fe1057e4bff5a1e2e24fed36d6bdba5e0078
hitohito1023 <ryotakinase1023@gmail.com>
2022-08-07 11:13:43 +0900

    [finish]lesson7

--- Lesson/lesson7.py
+++ Lesson/lesson7.py
@@ -26,11 +26,54 @@
 #   i += 1
 # print(tv)
 
-for i in range(1, 11):
-  print(i)
+# for i in range(1, 11):
+#   print(i)
 
-x = 10
-while x > 0:
-  print('{}'.format(x))
-  x -= 1
-print("Happy New Year!")
+# x = 10
+# while x > 0:
+#   print('{}'.format(x))
+#   x -= 1
+# print("Happy New Year!")
+
+# for i in range(1,6):
+#     if i == 3:
+#     	continue
+#     print(i)
+
+# for i in range(1, 3):
+# 	print(i)
+# 	for letter in ["a", "b", "c"]:
+# 		print(letter)
+
+# titles = ["ウォーキング・デッド", "アントラージュ", "ザ・ソプラノズ", "ヴァンパイア・ダイアリーズ"]
+# for title in titles:
+# 	print(title)
+
+# for i in range(25, 51):
+# 	print(i)
+
+# titles = ["ウォーキング・デッド", "アントラージュ", "ザ・ソプラノズ", "ヴァンパイア・ダイアリーズ"]
+# for i, title in enumerate(titles):
+# 	print(str(i) + ":" + title)
+
+# list = [3, 6, 8, 9]
+# while True:
+# 	print("qで終了します")
+# 	ans = input("数字を当てよう！！")
+# 	if ans == "q":
+# 		break
+# 	elif int(ans) in list:
+# 		print("正解です！！")
+# 	else:
+# 		print("残念！")
+
+list1 = [8, 19, 148, 4]
+list2 = [9, 1, 33, 83]
+ans_list = []
+
+for i in list1:
+	for j in list2:
+		ans = i * j
+		ans_list.append(ans)
+
+print(ans_list)
