4ee586450f45f835df13af37ce80b1961dc189c4
hitohito1023 <ryotakinase1023@gmail.com>
2022-08-07 11:45:38 +0900

    [finish]lesson8

---
+++ Lesson/hello.py
@@ -0,0 +1,5 @@
+def print_hello():
+  print("Hello")
+
+def cubed(x):
+  return x ** 3
---
+++ Lesson/lesson8.py
@@ -0,0 +1,23 @@
+import math
+import random
+import statistics
+import keyword
+import hello
+
+# print(random.randint(0,100))
+# print(math.pow(2, 3))
+
+nums = [1, 5, 33, 12, 46, 33, 2]
+# print(statistics.mean(nums))
+# print(statistics.median(nums))
+# print(statistics.mode(nums))
+
+# print(keyword.iskeyword("for"))
+# print(keyword.iskeyword("football"))
+
+# hello.print_hello()
+
+# print(statistics.median_low(nums))
+# print(statistics.median_high(nums))
+
+print(hello.cubed(3))
